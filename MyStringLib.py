b38588679a72a478815b615fbdfc5e4173e9976e
hansmolenaar <molenaar.johannes@gmail.com>
2026-09-23 12:49:36 +0200

    puntjes op de i

--- MyStringLib.py
+++ MyStringLib.py
@@ -30,9 +30,18 @@ def replaceOddCharacters(name):
     retval = re.sub("´" , "'", retval)
     retval = re.sub("’" , "'", retval)
     retval = re.sub("º" , "o", retval)
+    retval = re.sub("°" , "o", retval)
     retval = re.sub("ß" , "ss", retval)
+    retval = re.sub("İ" , "I", retval)
+    retval = re.sub("Ο" , "O", retval)
 
-    ignore = [220, 225, 228, 231, 232, 233, 237, 241,  246, 252]
+    retval = re.sub(str(chr(804)) , "", retval)
+    retval = re.sub(str(chr(769)) , "", retval) # accent grave
+    retval = re.sub(str(chr(776)) , "", retval) # trema
+    retval = re.sub(str(chr(351)) , "s", retval) # Turks: s cedille
+    retval = re.sub(str(chr(305)) , "i", retval) # Turks
+
+    ignore = [193, 195, 209, 214, 216, 227, 220, 225, 228, 231, 232, 233, 237, 241, 243, 246, 252]
     for c in retval:
        ordc = ord(c)
        if (ordc > 127) and not (ordc in ignore):
